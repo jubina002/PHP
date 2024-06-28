2fb38a858a2031059ca9d83603f39104f2ce258d
jubina002 <70877578+jubina002@users.noreply.github.com>
2024-06-28 23:04:20 +0545

    DAY 4

---
+++ Functions/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Built-in functions</title>
+</head>
+<body>
+    <?php
+
+    // String functions
+    // $string = "Hello World!";
+    // echo strlen($string); //12
+    // echo strpos($string, "Wo"); //4
+    // echo str_replace("World", "PHP", $string);
+    // echo strtolower($string);
+    // echo strtoupper($string);
+    // echo substr($string, 2 , -2);
+    // print_r(explode(" ", $string));
+
+    // Math functions
+    // $number = -5.5;
+    // echo abs($number);
+    // echo round($number);
+    // echo pow(3, 3);
+    // echo sqrt(4);
+    // echo rand(1, 100);
+
+    // Array functions
+    // $array = ["apple", "banana", "orange"];
+    // echo count($array);
+    // echo is_array($array);
+
+    // array_push($array, "kiwi");
+    // print_r($array);
+
+    // array_pop($array);
+    // print_r($array);
+
+    // print_r(array_reverse($array));
+
+    // $array1 = ["apple", "banana", "orange"];
+    // $array2 = ["kiwi", "mango", "peach"];
+    // print_r(array_merge($array1, $array2));
+
+    // Date Time functions
+    // echo date("Y-m-d H:i:s");
+    
+    // echo time();
+
+    // $date = "2024-20-28 10:24:49";
+    // echo strtotime($date);
+
+  
+    ?>
+</body>
+</html>
---
+++ Functions/u.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types= 1);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User defined function</title>
+</head>
+<body>
+    <?php
+
+    // function sayHello(){
+    //     return "Hello World!";
+    // }
+    // echo sayHello();
+    // $test = sayHello();
+    // echo $test;
+
+    // function sayHello($name, $lastname){
+    //         return "Hello " . $name . " " .$lastname. "!";
+    // }
+    
+    // $test = sayHello("Jubina", "Zubina");
+    //     echo $test;
+    
+//     function sayHello($name= "Jubina"){
+//         return "Hello " . $name . "!";
+// }
+
+// $test = sayHello("Zubina");
+//     echo $test;
+
+// function sayHello($name= "Jubina"){
+//     return "Hello " . $name . "!";
+// }
+
+// $test = sayHello("Zubina");
+// echo $test;
+
+// type declaration
+// function sayHello(string $name){
+//     return "Hello " . $name . "!";
+// }
+
+// $test = sayHello(123);
+// echo $test;
+// declare(strict_types= 1);
+
+// Calculator
+// $test = "Jubina";
+// function calculator(int $num1, int $num2){
+//     global $test;
+//     $result = $num1 + $num2;
+//     return $test;
+// }
+
+// $test = calculator(2, 5);
+// echo $test;
+
+
+
+    ?>
+</body>
+</html>
